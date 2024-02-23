f351e79e463fbf118ffe156751fe9b8be9b8f986
boldak <boldak.andrey@gmail.com>
2024-02-23 18:19:43 +0200

    fix

--- src/scripts/v3-manual-wf-ecg.js
+++ src/scripts/v3-manual-wf-ecg.js
@@ -1,68 +1,72 @@
 const googledriveService = require("../utils/google-drive")
 const { extend, sortBy, find, truncate, groupBy, keys, isUndefined, last, first } = require("lodash")
-const { loadJSON, unlink, writeFile, saveJSON } = require("../utils/file-system")
+const { loadJSON, saveJSON, unlink, writeFile } = require("../utils/file-system")
 const { saveXLSX, loadXLSX } = require("../utils/xlsx")
 const { avg, std, anomals } = require("../utils/stat")
 
-const R = require('../utils/R');
-
-const { fitSegmentations } = require("../utils/fit-segments")
 
 
 const ST_JSON = "./data/v3/ai/v3pxecg-iph.json"
-const ST_FW_JSON = "./data/structures/V3-AI-Validation-2024/WF/v3pxecg-iph-waveform.json"
-const EKO_FW_JSON = "./data/structures/V3-AI-Validation-2024/WF/v3pxecg-ekocore-waveform.json"
 
-const RESULT_JSON = "./data/structures/V3-AI-Validation-2024/10.2.1 Test 2.1. Validation of heart sounds timing by Stethophone in clinical environment/waveform-pool.json"
-// const GD_ROOT = "V3-AI-Validation-2024/10.2.1 Test 2.1. Validation of heart sounds timing by Stethophone in clinical environment"
+const ST_WF_JSON = "./data/structures/V3-AI-Validation-2024/WF/v3pxecg-iph-waveform.json"
+const EKO_WF_JSON = "./data/structures/V3-AI-Validation-2024/WF/v3pxecg-ekocore-waveform.json"
+
+
+const RESULT_JSON = "./data/structures/V3-AI-Validation-2024/10.2.1 Test 2.1. Validation of heart sounds timing by Stethophone in clinical environment/waveforms.json"
+const GD_ROOT = "V3-AI-Validation-2024/10.2.1 Test 2.1. Validation of heart sounds timing by Stethophone in clinical environment"
 
 const getDrive = async path => {
 	let googleDrive = await googledriveService.create(path)
 	return googleDrive	 
 }
 
+
+
+
 const run = async () => {
 
-	let st_data = loadJSON(ST_JSON)
-	let st_wf = loadJSON(ST_FW_JSON)
+	let st_metadata = loadJSON(ST_JSON)
+	let st_wf = loadJSON(ST_WF_JSON)
+	let eko_wf = loadJSON(EKO_WF_JSON)
 	
-	st_data = st_data.map( d => ({
+	st_metadata = st_metadata.map( d => ({
 		patientId: last(d.patient_id.split("-")),
 		file: d.file_id
 	}))
 	
-	let result = [] 
+	let result = st_wf.map(d=> {
+		d.metadata.device = "Stethophone",
+		console.log(d.metadata.fileName)
+		let f = find(st_metadata, s => {
+				return `${s.file}.wav` == d.metadata.fileName
+			})
+		d.metadata.patientId = (f) ? f.patientId :  undefined
+		return d
+	})
+
+	result = result.filter(d => d.metadata.patientId) 
+
+	result = result.concat( 
+		eko_wf.map(d => {
+			d.metadata.device = "Eko CORE"
+			d.metadata.patientId = d.metadata.fileName.split("-")[4]
+			return d
+		})
+	)
 	
-	st_data.forEach( d => {
-		result.push ({
-			device: "Stethophone",
-			patientId: d.patientId,
-			waveform: find(st_wf, wf => `${d.file}.wav` == wf.filename).waveform
-		})
-	})
-
-
-	let eko_wf = loadJSON(EKO_FW_JSON)
-
-	eko_wf.forEach( d => {
-		result.push({
-			device: "Eko CORE",
-			patientId: d.filename.split("-")[4],
-			waveform: d.waveform
-		})
-	})
-
+	
+	console.log(`Save ${RESULT_JSON}`)
+	
 	saveJSON(RESULT_JSON, result)
 
+	let drive = await getDrive(`${GD_ROOT}`)
 	
-	// let drive = await getDrive(`${GD_ROOT}`)
+	console.log(`Upload to: ${GD_ROOT}/waveforms.json`)
 	
-	// console.log(`Upload to: ${GD_ROOT}/segments-pool.xlsx`)
-	
-	// await drive.uploadFiles({
-	// 	fs: [RESULT_XLSX],
-	// 	googleDrive: GD_ROOT
-	// })
+	await drive.uploadFiles({
+		fs: [RESULT_JSON],
+		googleDrive: GD_ROOT
+	})
 
 	
 }
--- src/scripts/v3-wf-zip.js
+++ src/scripts/v3-wf-zip.js
@@ -76,7 +76,7 @@ const run = async options => {
 	let res = await wav2waveform({
 		fs: `${TEMP}/wav/*.wav`,
 		metadata: { message: ""}, 
-		params: { tick: 0.01} 
+		params: { tick: 0.005, ratio:2 } 
 	})
 
 	res = res.map( r => ({
