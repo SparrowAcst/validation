ad315717a3bcabf3b3dad18bfa2df73516813c73
boldak <boldak.andrey@gmail.com>
2024-12-13 01:48:50 +0200

    add cross-tables

--- src/migrate-docdb/migrations/test.js
+++ src/migrate-docdb/migrations/test.js
@@ -2,8 +2,8 @@ module.exports = [
 
     /////////////////////////////////////////////////////////////////////////////
     {
-        source: "DEV-CLINIC-TEST.labels",
-        dest: `DEV-CLINIC-TEST.labels`,
+        source: "strazhesko-part-1-done.labels",
+        dest: `strazhesko-part-1.labels`,
         pipeline: [
             {
                 $project: {
@@ -14,21 +14,17 @@ module.exports = [
     },
 
 
-    // {
-    //     source: "sparrow.H2-EXAMINATION",
-    //     dest: `strazhesko-part-1-1.examinations`,
-    //     pipeline: [{
-    //             $match: {
-    //                 siteId: "c42ac1bd-ae37-4a47-b431-44cf4d886be1"
-    //             }
-    //         },
-    //         {
-    //             $project: {
-    //                 _id: 0
-    //             }
-    //         }
-    //     ]
-    // },
+    {
+        source: "strazhesko-part-1-done.examinations",
+        dest: `strazhesko-part-1.examinations`,
+        pipeline: [
+            {
+                $project: {
+                    _id: 0
+                }
+            }
+        ]
+    },
 
 
 
