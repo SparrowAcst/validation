7ac8ef0330ebdaef4d85f1c3edc74e3178256710
boldak <boldak.andrey@gmail.com>
2024-12-13 01:37:04 +0200

    add cross-tables

--- src/transform/stages/clean.js
+++ src/transform/stages/clean.js
@@ -4,15 +4,15 @@ const migrations = require("./migrations/clean")
 
 
 const schemas = [
-	"potashev-part-1",
-	"denis-part-1",
-	"yoda",
-	"harvest1",
-	"phonendo",
+	// "potashev-part-1",
+	// "denis-part-1",
+	// "yoda",
+	// "harvest1",
+	// "phonendo",
 	"strazhesko-part-1",
-	"poltava-part-1",
-	"digiscope",
-	"hha"
+	// "poltava-part-1",
+	// "digiscope",
+	// "hha"
 ] 
 
 const PREFIX = "-mix"
