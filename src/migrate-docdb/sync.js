2b5228563f311b8a36d75d71bed83c6c95b597e1
boldak <boldak.andrey@gmail.com>
2024-12-13 21:32:12 +0200

    add cross-tables

--- src/migrate-docdb/sync.js
+++ src/migrate-docdb/sync.js
@@ -1,6 +1,6 @@
 const execute = require("./utils/sync")
 
-const COLLECTION = "strazhesko-part-1.labels"
+const COLLECTION = "strazhesko-part-1.examinations"
 
 const run = async () => {
   await execute(COLLECTION)
